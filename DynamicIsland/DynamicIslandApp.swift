189bf62616c3aaaabce63163ef5542f3dc5548d3
Seungpyo <rush94434@gmail.com>
2025-10-21 09:55:06 +0900

    Initial Commit

---
+++ DynamicIsland/DynamicIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicIslandApp.swift
+//  DynamicIsland
+//
+//  Created by 홍승표 on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DynamicIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
